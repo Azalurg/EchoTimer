8dcf542781ccb2c1590848faa610aa7b19c1024d
Azalurg <91213389+Azalurg@users.noreply.github.com>
2023-11-23 10:26:16 +0100

    Add sound

---
+++ README.md
@@ -0,0 +1,5 @@
+https://www.youtube.com/watch?v=LZkqvIsl6wc
+
+https://www.youtube.com/watch?v=sXQKxwABl0o
+
+https://www.youtube.com/watch?v=i0DON3AjhW4
--- script.js
+++ script.js
@@ -7,27 +7,30 @@ let stop_button = document.getElementById("timer_buttons_stop");
 let reset_button = document.getElementById("timer_buttons_reset");
 let skip_button = document.getElementById("timer_buttons_skip");
 
+let bell_button = document.getElementById("bell_button");
+
 let work_time = 40;
 let break_time = 10;
-let seconds = "00";
+let seconds = 0;
 let long_break_time = 30;
 
 work = true;
-start = false;
 
 interval = null;
+audio = null;
 
 window.onload = () => {
     timer_time_minutes.innerHTML = work_time;
-    timer_time_seconds.innerHTML = seconds;
+    timer_time_seconds.innerHTML = String(seconds).padStart(2, '0');
 
     stop_button.style = "display: none;";
+    bell_button.style = "display: none;";
 }
 
 function startTimer() {
+    clickSound();
     start_button.style = "display: none;";
     stop_button.style = "display: inline;";
-    start = true;
     interval = setInterval(timer, 1000);
     if (work) {
         timer_title.innerHTML = "Work";
@@ -37,19 +40,20 @@ function startTimer() {
 }
 
 function stopTimer() {
+    clickSound();
     timer_title.innerHTML = "Paused";
     clearInterval(interval);
-    start = false;
     start_button.style = "display: inline;";
     stop_button.style = "display: none;";
 }
 
 function resetTimer() {
+    clickSound();
     clearInterval(interval);
-    start = false;
     start_button.style = "display: inline;";
     stop_button.style = "display: none;";
-    timer_time_seconds.innerHTML = seconds;
+    seconds = 0;
+    timer_time_seconds.innerHTML = String(seconds).padStart(2, '0');
     if (work) {
         timer_title.innerHTML = "Work";
         timer_time_minutes.innerHTML = work_time;
@@ -60,14 +64,15 @@ function resetTimer() {
 }
 
 function skipTimer() {
+    clickSound();
     work = !work;
     resetTimer();
 }
 
 timer = () => {
-    if (seconds == "00") {
+    if (seconds == 0) {
         seconds = 59;
-        timer_time_seconds.innerHTML = seconds;
+        timer_time_seconds.innerHTML = String(seconds).padStart(2, '0');
         timer_time_minutes.innerHTML = timer_time_minutes.innerHTML - 1;
     } else {
         seconds = seconds - 1;
@@ -77,12 +82,41 @@ timer = () => {
     if (timer_time_minutes.innerHTML == 0 && seconds == 0) {
         if (work) {
             timer_time_minutes.innerHTML = break_time;
-            timer_time_seconds.innerHTML = seconds;
+            timer_time_seconds.innerHTML = String(seconds).padStart(2, '0');
             work = false;
         } else {
             timer_time_minutes.innerHTML = work_time;
-            timer_time_seconds.innerHTML = seconds;
+            timer_time_seconds.innerHTML = String(seconds).padStart(2, '0');
             work = true;
         }
+        playSound();
+        resetTimer();
     }
 }
+
+// Sound
+
+function playSound() {
+    bell_button.style = "display: inline;";
+    if (!work) {
+        audio = new Audio("./mp3/intro.mp3");
+        
+    } else {
+        audio = new Audio("./mp3/outro.mp3");
+    }
+    audio.play();
+}
+
+function stopSound() {
+    clickSound();
+    audio.pause();
+    audio.currentTime = 0;
+    bell_button.style = "display: none;";
+}
+
+function clickSound() {
+    audio = new Audio("./mp3/click.mp3");
+    audio.play();
+}
+
+
